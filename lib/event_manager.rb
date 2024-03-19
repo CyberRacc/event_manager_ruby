09eecd1f1d57cc70037894e2b0d5d46f8249f55a
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-03-19 23:06:45 +0000

    Add form_letter.html and update event_manager.rb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,41 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  )
+
+  legislators.officials.map(&:name).join(', ')
+rescue StandardError => e
+  puts "An error occurred: #{e.message}"
+  puts 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+end
+
 puts 'EventManager initialized.'
 
-lines = File.readlines('event_attendees.csv')
-lines.each do |line|
-  columns = line.split(',')
-  p columns
+template_letter = File.read('form_letter.html')
+
+contents = CSV.open('event_attendees.csv',
+                    headers: true,
+                    header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
