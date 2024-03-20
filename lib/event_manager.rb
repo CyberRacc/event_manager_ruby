5e539a1c1ee977dda2924886e9065f86df5457c8
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-03-20 16:28:45 +0000

    Add phone number cleaning and thank you letter saving functionality

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,40 +2,64 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  if phone_number < 10
+    'Bad number'
+  elsif phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..10]
+  else
+    'Bad number'
+  end
 end
 
 def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-  legislators = civic_info.representative_info_by_address(
-    address: zipcode,
-    levels: 'country',
-    roles: %w[legislatorUpperBody legislatorLowerBody]
-  )
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    puts 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
 
-  legislators.officials.map(&:name).join(', ')
-rescue StandardError => e
-  puts "An error occurred: #{e.message}"
-  puts 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts 'EventManager initialized.'
 
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
 
 contents = CSV.open('event_attendees.csv',
                     headers: true,
                     header_converters: :symbol)
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  save_thank_you_letter(id, form_letter)
 end
